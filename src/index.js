8c1842226438867b906dd8c2d26eaf65bc128687
Taichiro Yoshida <taichiro.yoshida@gmail.com>
2019-06-18 15:16:27 +0900

    Enable sandbox option

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      sandbox: true
+    }
   });
 
   // and load the index.html of the app.
